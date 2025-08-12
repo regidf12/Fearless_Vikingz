4f74b40d25b1144f95060d4281e83c502ab24067
regidf12 <tiltedfear@gmail.com>
2025-08-12 20:58:59 +0300

    Отредактировал пути и наименование модуля

--- fearlessvikingz/fearlessvikingz/wsgi.py
+++ fearlessvikingz/fearlessvikingz/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for taskmanager project.
+WSGI config for fearlessvikingz project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fearlessvikingz.settings')
 
 application = get_wsgi_application()
